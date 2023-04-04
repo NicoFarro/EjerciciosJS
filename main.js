73595f1a55ed155f93b21b5ee5d06ecae28dd7cc
NicoFarro <nicofg.97@gmail.com>
2023-04-03 22:35:07 -0300

    Agregado de array de Los Simpsons

---
+++ main.js
@@ -0,0 +1,32 @@
+const familia = {
+    nombre: 'Simpsons',
+    nacionalidad: 'Estados Unidos',
+    miembros: [
+{
+    nombre: 'Homero',
+    edad: 40,
+    rol: 'Padre'
+},
+{
+    nombre: 'Marge',
+    edad: 38,
+    rol: 'Madre'
+},
+{
+    nombre: 'Bart',
+    edad: 10,
+    rol: 'Hijo'
+},
+{
+    nombre: 'Lisa',
+    edad: 8,
+    rol: 'Hija'
+},
+{
+    nombre: 'Maggie',
+    edad: 2,
+    rol: 'Bebe'
+}
+]
+}
+console.log(familia.miembros);
